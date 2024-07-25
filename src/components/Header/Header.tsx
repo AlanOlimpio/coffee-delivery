ca313d8e3d393efeb61a1cb19e418ebb2cfd9831
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-07-25 11:49:07 -0300

    fix: corrige DefaultLayout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,27 +4,30 @@ import coffeeDeliveryLogo from '../../assets/coffee-delivery-logo.svg';
 import Button from '../Button';
 import { defaultTheme } from '../../styles/themes/default';
 import Location from '../Location';
+import { LayoutContainer } from '../../layouts/DefaultLayoutStyled';
 
 function Header() {
   return (
-    <HeaderContainer>
-      <img src={coffeeDeliveryLogo} alt="Coffee Delivery" />
-      <HeaderwrapperRight>
-        <Location />
-        <Button
-          $gap="0.25rem"
-          $backgroundColor="yellow-light"
-          $paddingY="0.5rem"
-          $paddingX="0.5rem"
-        >
-          <ShoppingCart
-            color={defaultTheme.font.color['yellow-dark']}
-            size={22}
-            weight="fill"
-          />
-        </Button>
-      </HeaderwrapperRight>
-    </HeaderContainer>
+    <LayoutContainer>
+      <HeaderContainer>
+        <img src={coffeeDeliveryLogo} alt="Coffee Delivery" />
+        <HeaderwrapperRight>
+          <Location />
+          <Button
+            $gap="0.25rem"
+            $backgroundColor="yellow-light"
+            $paddingY="0.5rem"
+            $paddingX="0.5rem"
+          >
+            <ShoppingCart
+              color={defaultTheme.font.color['yellow-dark']}
+              size={22}
+              weight="fill"
+            />
+          </Button>
+        </HeaderwrapperRight>
+      </HeaderContainer>
+    </LayoutContainer>
   );
 }
 export default Header;
--- src/layouts/DefaultLayoutStyled.ts
+++ src/layouts/DefaultLayoutStyled.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
+export const containerMaxWidth = '70rem';
+
 export const LayoutContainer = styled.div`
-  max-width: 70rem;
-  padding: 0 0 9.813rem;
+  max-width: ${containerMaxWidth};
   margin: 0 auto;
   border-radius: 8px;
   display: block;
@@ -10,12 +11,12 @@ export const LayoutContainer = styled.div`
   flex-direction: column;
   align-items: center;
   @media screen and (min-width: 1024px) and (max-width: 1365px) {
-    padding: 0 5rem 5rem;
+    padding: 0 5rem;
   }
   @media screen and (min-width: 769px) and (max-width: 1023px) {
-    padding: 0 2rem 2rem;
+    padding: 0 2rem;
   }
   @media screen and (max-width: 768px) {
-    padding: 0 1rem 1rem;
+    padding: 0 1rem;
   }
 `;
