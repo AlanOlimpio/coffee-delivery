1dd68e334a6325b7ae4fac758a1152636e8eaaf8
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-08-05 17:23:39 -0300

    feat: adiciona badges count

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,22 @@
 import { ShoppingCart } from 'phosphor-react';
-import { HeaderContainer, HeaderwrapperRight } from './HeaderStyled';
+import {
+  HeaderButton,
+  HeaderContainer,
+  HeaderwrapperRight,
+} from './HeaderStyled';
 import coffeeDeliveryLogo from '../../assets/coffee-delivery-logo.svg';
 import Button from '../Button';
 import { defaultTheme } from '../../styles/themes/default';
 import Location from '../Location';
 import { LayoutContainer } from '../../layouts/DefaultLayoutStyled';
 import { useNavigate } from 'react-router-dom';
+import { CoffeeContext } from '../../contexts/CoffeeContext';
+import { useContext } from 'react';
 
 function Header() {
   const navigate = useNavigate();
+
+  const { cartList } = useContext(CoffeeContext);
   return (
     <LayoutContainer>
       <HeaderContainer>
@@ -21,21 +29,24 @@ function Header() {
         />
         <HeaderwrapperRight>
           <Location />
-          <Button
-            $gap="0.25rem"
-            $backgroundColor="yellow-light"
-            $paddingY="0.5rem"
-            $paddingX="0.5rem"
-            $onClick={() => {
-              navigate('/checkout');
-            }}
-          >
-            <ShoppingCart
-              color={defaultTheme.font.color['yellow-dark']}
-              size={22}
-              weight="fill"
-            />
-          </Button>
+          <HeaderButton>
+            <Button
+              $gap="0.25rem"
+              $backgroundColor="yellow-light"
+              $paddingY="0.5rem"
+              $paddingX="0.5rem"
+              $onClick={() => {
+                navigate('/checkout');
+              }}
+            >
+              <ShoppingCart
+                color={defaultTheme.font.color['yellow-dark']}
+                size={22}
+                weight="fill"
+              />
+            </Button>
+            {cartList.length > 0 && <span>{cartList.length}</span>}
+          </HeaderButton>
         </HeaderwrapperRight>
       </HeaderContainer>
     </LayoutContainer>
--- src/components/Header/HeaderStyled.ts
+++ src/components/Header/HeaderStyled.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const HeaderContainer = styled.header`
   display: flex;
@@ -18,4 +18,25 @@ export const HeaderwrapperRight = styled.header`
   gap: 0.75rem;
   justify-content: space-between;
   flex-wrap: wrap;
+`;
+
+export const HeaderButton = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    span {
+      position: absolute;
+      top: -0.5rem;
+      right: -0.5rem;
+      font-size: ${theme.font.size.text.xs};
+      font-weight: bold;
+      color: ${theme.font.color['white']};
+      background: ${theme.font.color['yellow-dark']};
+      width: 1.25rem;
+      height: 1.25rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+    }
+  `}
 `;
